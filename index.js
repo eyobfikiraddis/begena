b53968757d8e71d424dfe5a6f90f816508094de7
eyobfikiraddis <aeyobafikir@gmail.com>
2025-09-29 17:00:07 +0300

    daily commit

--- index.js
+++ index.js
@@ -9,53 +9,21 @@ for (var i = 0; i < n; i++){
 
 document.addEventListener("keydown", function(event) {
     var x = event.key;
-    var y = "";
-    if(x === "a"){
-        y = "G";
-    }
-    else if (x === "e"){
-        y = "A";
-    }
-    else if (x === "r"){
-        y = "D";
-    }
-    else if(x === "t"){
-        y = "C";
-    }
-    else{
-        y = "F"
-    }
+    var y = keyMap[x];
     whenclicked(y);
     buttonAnimation(y);
     
 });
 
 function whenclicked(key){
-    switch (key) {
-        case "F":
-            var w = new Audio("sounds/F.m4a");
-            w.play();
-            break;
-        case "C":
-            var a = new Audio("sounds/C.m4a");
-            a.play();
-            break;
-        case "D":
-            var s = new Audio("sounds/D.m4a");
-            s.play();
-            break;
-        case "A":
-            var d = new Audio("sounds/A.m4a");
-            d.play();
-            break;
-        case "G":
-            var j = new Audio("sounds/G.m4a");
-            j.play();
-            break;
-        default:
-            console.log(buttoninnerhtml);
+    if (key === "Gs"){
+        var v = new Audio("sounds/" + key + ".mp3");
+        v.play();
     }
-}
+    else {
+    var w = new Audio("sounds/" + key + ".m4a");
+    w.play();
+}}
 function buttonAnimation(currentKey) {
     var activeButton = document.querySelector("." + currentKey);
     activeButton.classList.add("pressed");
